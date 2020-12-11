b0e93bc036e7297bb89dcf4ce97ca0d8a889c5a7
nigelrawlins-rp <nigelrawlins@hotmail.com>
2020-12-11 02:03:47 +0000

    Refactor of summary component changes.

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
-import getExpensesTotal from '../selectors/expenses-total';
+import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
-    <div>
-        {
-            props.expenses.length === 1 ? (
-                <p>Viewing 1 expense totalling {numeral(getExpensesTotal(props.expenses) / 100).format('$0,0.00')}</p>
-            ) : (
-                <p>Viewing {props.expenses.length} expenses totalling {numeral(getExpensesTotal(props.expenses) / 100).format('$0,0.00')}</p>
-            )
-        }
-    </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+        </div>
+    )
+};
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     };
 };
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -4,11 +4,11 @@ import { ExpensesSummary } from '../../components/ExpensesSummary.js';
 import expenses from '../fixtures/expenses';
 
 test('should render ExpensesSummary with one expense', () => {
-    const wrapper = shallow(<ExpensesSummary expenses={[expenses[1]]}/>);
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should render ExpenseSummary with multiple expenses', () => {
-    const wrapper = shallow(<ExpensesSummary expenses={expenses}/>);
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={23512340987} />);
     expect(wrapper).toMatchSnapshot();
 });
